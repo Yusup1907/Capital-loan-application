130ce8f7ebadb64369982cbaf5d56f325aa4f83c
DINO <masbagus1581@gmail.com>
2023-07-15 17:29:17 +0700

    [FETURE]update validate

--- go.mod
+++ go.mod
@@ -3,23 +3,21 @@ module pinjam-modal-app
 go 1.20
 
 require (
-	
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/validator/v10 v10.14.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.9.0
 )
 
 require (
-	
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
--- model/user_model.go
+++ model/user_model.go
@@ -1,15 +1,36 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type UserModel struct {
-	Id          int        `json:"id"`
-	UserName    string     `json:"username"`
-	Email       string     `json:"email"`
-	Password    string     `json:"password"`
-	RolesName   string     `json:"roles_name"`
+	Id          int        `json:"id" validate:"required"`
+	UserName    string     `json:"username" validate:"required"`
+	Email       string     `json:"email" validate:"required,email"`
+	Password    string     `json:"password" validate:"requiredmin=12,max=12"`
+	RolesName   string     `json:"roles_name" validate:"required"`
 	IsActive    bool       `json:"is_active"`
-	PhoneNumber string     `json:"phone_number"`
+	PhoneNumber string     `json:"phone_number" validate:"required,min=13,max=13"`
 	CreatedAt   *time.Time `json:"created_at"`
 	UpdatedAt   *time.Time `json:"updated_at"`
+}
+
+func (c *UserModel) Validate() error {
+	validate := validator.New()
+	err := validate.Struct(c)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		errMsg := ""
+		for _, e := range errs {
+			errMsg += fmt.Sprintf("Field %s: validation failed on tag '%s'\n", e.Field(), e.Tag())
+		}
+		return errors.New(errMsg)
+	}
+
+	return nil
 }
