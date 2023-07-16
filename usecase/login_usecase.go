a903797dc84fa0018079dd1e173a58435ecdbe9b
DINO <masbagus1581@gmail.com>
2023-07-16 18:20:03 +0700

    Commit usecase/login_usecase.go

--- usecase/login_usecase.go
+++ usecase/login_usecase.go
@@ -1,9 +1,12 @@
 package usecase
 
 import (
+	"fmt"
 	"pinjam-modal-app/repository"
+	utils "pinjam-modal-app/utils/authutil"
 
 	"github.com/gin-contrib/sessions"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,27 +20,28 @@ type loginUsecase struct {
 	loginRepo repository.LoginRepo
 }
 
-// func (lu *loginUsecase) Login(email, password string) (string, error) {
-// 	// Mengecek apakah pengguna dengan email tersebut ada di penyimpanan data
-// 	user, err := lu.loginRepo.GetUserByEmail(email)
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to retrieve user: %v", err)
-// 	}
+func (lu *loginUsecase) Authenticate(email, password string) (string, error) {
+	// Mengecek apakah pengguna dengan email tersebut ada di penyimpanan data
+	user, err := lu.loginRepo.GetUserByEmail(email)
+	if err != nil {
+		return "", fmt.Errorf("failed to retrieve user: %v", err)
+	}
 
-// 	// Verifikasi password pengguna dengan menggunakan bcrypt
-// 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-// 	if err != nil {
-// 		return "", fmt.Errorf("invalid email or password")
-// 	}
+	// Verifikasi password pengguna dengan menggunakan bcrypt
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "", fmt.Errorf("invalid email or password")
+	}
 
-// 	// Menghasilkan token JWT
-// 	token, err := utils.GenerateToken(user.UserName)
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to generate token: %v", err)
-// 	}
+	// Menghasilkan token JWT
+	token, err := utils.GenerateToken(user)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate token: %v", err)
+	}
 
-//		return token, nil
-//	}
+	return token, nil
+}
+
 func (lu *loginUsecase) Logout(ctx *gin.Context) error {
 	session := sessions.Default(ctx)
 	session.Clear()
